b5bea812c8ed9ab964e3fe92ff0447de910a3bc0
Alecio-ds <contatoalecio@outlook.com>
2025-03-04 18:47:19 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomeParticipantes = [];
+
+function adicionarAmigo() {
+    let input = document.getElementById("amigo");
+    let nome = input.value.trim();
+    console.log(nome);
+    
+        if (nome == ""){
+            alert("Por favor, insira um nome.");
+        return;
+
+        }
+    
+        if(nomeParticipantes.includes(nome)){
+            alert("Este nome já foi adicionado!");
+            return;
+
+        }
+
+        nomeParticipantes.push(nome);
+        console.log(nomeParticipantes);
+        atualizarLista();
+        input.value = "";
+             
+}
+
+function atualizarLista(){
+    let listaParticipantes = document.getElementById("listaAmigos");
+    listaParticipantes.innerHTML = "";
+
+    nomeParticipantes.forEach(function(nome) {
+        let nomeNaTela = document.createElement("li");
+        nomeNaTela.textContent = nome;
+        listaParticipantes.appendChild(nomeNaTela);
+    });
+
+}
+
+function sortearAmigo(){
+    if (nomeParticipantes.length <= 1){
+        alert ("Insira no mínimo 2 nomes para que o sorteio funcione!");
+        return;
+    }
+    let indiceSorteio = Math.floor(Math.random() * nomeParticipantes.length);
+    let sorteado = nomeParticipantes [indiceSorteio];
+    document.getElementById("listaAmigos").innerHTML = "";
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML =  `Amigo Secreto: ${sorteado}`;
+
+}
